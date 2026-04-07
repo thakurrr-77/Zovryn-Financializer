adde4ea959ed71cc3fea92bd439b4c885f9adce6
Shubham Thakur <shubhamrthakur081@gmail.com>
2026-04-07 10:00:37 +0530

    sample commit to only deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -12,7 +12,7 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  return config;
+  return config; 
 });
 
 export default api;
